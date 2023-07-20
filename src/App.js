cba362053af2c794cf51304bb67dc9ccddee0ca3
Jatin <coderjatin123@gmail.com>
2023-07-20 19:15:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import BpkCalendar, { CALENDAR_SELECTION_TYPE } from '@skyscanner/backpack-web/bpk-component-calendar';
+import BpkInput, {
+  INPUT_TYPES,
+} from '@skyscanner/backpack-web/bpk-component-input';
+import format from 'date-fns/format';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const formatDateFull = (date) => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = (date) => format(date, 'MMMM yyyy');
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+  // ...
+];
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+      },
+    };
+  }
+
+  handleDateSelect = (date) => {
+    const { type } = this.state.selectionConfiguration || { type: CALENDAR_SELECTION_TYPE.single };
+    this.setState({
+      selectionConfiguration: {
+        ...this.state.selectionConfiguration,
+        type: type,
+        date: date,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className='App-header'>
+
+        <h2 className='heading'>
+        Flight Schedule.
+        </h2>
+        <BpkInput
+        className='calender'
+          id="dateInput"
+          type={INPUT_TYPES.text}
+          name="date"
+          value={(this.state.selectionConfiguration.date || '').toString()}
+          placeholder="Departure date"
+        />
+        <BpkCalendar
+        className='cal-sel'
+          id="calendar"
+          onDateSelect={this.handleDateSelect}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          daysOfWeek={daysOfWeek}
+          weekStartsOn={1}
+          changeMonthLabel="Change month"
+          nextMonthLabel="Next month"
+          previousMonthLabel="Previous month"
+          selectionConfiguration={this.state.selectionConfiguration}
+        />
+
+        <button className='button-10'>
+          Continue
+        </button>
+      </div>
+    );
+  }
+}
